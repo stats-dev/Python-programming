256c1109939b19537181863b4504ffdee33c598e
hohun94 <livekhh@cau.ac.kr>
2022-08-27 22:49:46 +0900

    git email 변경하여 커밋하기

--- test0827_2.py
+++ test0827_2.py
@@ -1,2 +1,21 @@
+N = int(input())
 
-test1
+
+test = []
+dp = [0] * N ## 0으로 변경해보면, 아래는 값이 바껴야 한다.
+
+for i in range(N):
+    A,B = map(int, input().split())
+    test.append((A,B))
+
+
+arr = sorted(test, key=lambda tup: tup[0])
+# print(test)
+# print(arr)
+
+for i in range(N):
+    for j in range(i):
+        if arr[i][1] > arr[j][1]: ## 오른쪽 전못대를 기준으로 가져간다.
+            dp[i] = max(dp[i], dp[j]+1)
+        
+print(N-max(dp)-1)
